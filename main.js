5ef705085a5c5e316454726ee1acaf355bbea4ba
riswan5567 <rasalbasheer35@gmail.com>
2025-04-06 14:20:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -74,8 +74,8 @@ const { autoStatusCommand, handleStatusUpdate } = require('./commands/autostatus
 // Global settings
 global.packname = settings.packname;
 global.author = settings.author;
-global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
-global.ytch = "Mr Unique Hacker";
+global.instaLink = "https://www.instagram.com/_riiiswaaan_?igsh=MWJvdmdvbncwZTRqcQ==";
+global.ID = " _riiiswaaan_ ";
 
 // Add this near the top of main.js with other global configurations
 const channelInfo = {
