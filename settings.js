eae23517489bae49642dd2cc1114d7e90372cdfd
riswan5567 <rasalbasheer35@gmail.com>
2025-04-06 13:14:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "_MALFO_BOT_",
+  botOwner: 'Riswan', // Your name
+  ownerNumber: '919526862271', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
